1cfb2a93ad0017eb93b0762641b9b7946b42a8a2
MoonlightUniverse <MoonlightUniverse@gmail.com>
2017-06-10 16:48:52 +0530

    ForTest
    Trying first time

---
+++ AppUnderTest.java
@@ -0,0 +1,10 @@
+package javaTestNG;
+
+public class AppUnderTest{
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Name\t:Chandrakant");
+		System.out.println("Message\t:Hello Chandrakant");
+		System.out.println("Class\t:Automation");
+		}
+}
